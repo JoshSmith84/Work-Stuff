f893c662f4e665e298c8f65a2f7681a23c8ac1b9
JoshSmith84 <jas426@icloud.com>
2022-10-12 14:11:49 -0500

    amp output parsing step 1- complete

--- amp_output_pull.py
+++ amp_output_pull.py
@@ -5,26 +5,42 @@
 import win32com.client
 import re
 
-def amp_output_pull(outdest, olreadfolder, olprocessedfolder):
+
+def amp_output_pull(out_folder: str, in_ofolder: str, out_ofolder: str) -> None:
+    """
+    Function to download all attachments in a folder in Outlook (in_ofolder).
+    The item index below may be unique to outlook. One must refactor this
+    and figure out the index of their mailbox in Outlook
+    especially if there are multiple accounts.
+
+    Once found, the attachment is downloaded to the out_folder param,
+    and the email is moved from in_ofolder param to the out_ofolder param
+
+    Needed imports to use this: win32.client, re
+
+    :param out_folder: Folder on local PC to save attachments to
+    :param in_ofolder: Outlook folder to search incoming emails.
+    :param out_ofolder: Outlook folder to send email after it is processed
+    """
+    # Variable initialization
     outlook = win32com.client.Dispatch(
         "Outlook.Application").GetNamespace("MAPI")
-    inbox = outlook.Folders.Item(3).Folders['Inbox'].Folders[olreadfolder]
+    inbox = outlook.Folders.Item(3).Folders['Inbox'].Folders[in_ofolder]
     outbox = outlook.Folders.Item(3).Folders[
-        'Inbox'].Folders[olprocessedfolder]
+        'Inbox'].Folders[out_ofolder]
     messages = inbox.Items
-    # changed this to use regex to detect file type.
+    # regex looking for .zip files
     amp_out_regex = re.compile(r"""^(.*?)(\.)(zip)$""")
+    # iterate through all emails in the folder
     for msg in list(messages):
+        # Iterate through all attachments in each email
         for atmt in msg.Attachments:
-            print(str(atmt))
             mo = re.search(amp_out_regex, str(atmt))
             if mo:
-                temp_filename = outdest + msg.Subject + '_' + atmt.FileName
+                # If found, save attachment and move email
+                temp_filename = out_folder + msg.Subject + '_' + atmt.FileName
                 atmt.SaveAsFile(temp_filename)
                 print('File Successfully Saved [{}]'.format(temp_filename))
                 msg.Move(outbox)
 
 
-folder = 'C:\\temp\\test\\'
-
-amp_output_pull(folder, 'Auto Policy', 'Processed')
