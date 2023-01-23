22f0c60c667f522c157d17fb6365024ae854c482
JoshSmith84 <jas426@icloud.com>
2023-01-23 16:37:01 -0600

    Cleaned up conflict/unanimous check block

--- mime-ess-new.py
+++ mime-ess-new.py
@@ -13,8 +13,22 @@ import tkinter as tk
 from tkinter import filedialog
 import os
 import time
-from remove_dup import remove_dup
 
+
+def remove_dup(a: list) -> None:
+    """Remove duplicate strings from a given list.
+
+    :param a: List to iterate through
+    """
+    i = 0
+    while i < len(a):
+        j = i + 1
+        while j < len(a):
+            if a[i] == a[j]:
+                del a[j]
+            else:
+                j += 1
+        i += 1
 
 
 # Open needed files and bind to variables
